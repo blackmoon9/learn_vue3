41e9f5259bc2f604280ea5ad4a162c7efbaf9b20
liuguosheng <17621831561@163.com>
2022-04-11 16:54:51 +0800

    01

---
+++ src/01_动画的基本使用/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <button @click="isShow = !isShow">显示/隐藏</button>
+  <transition name="msg">
+    <h2 v-if="isShow">Hello jack！！！！</h2>
+  </transition>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      isShow:true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .msg-enter-from,
+  .msg-leave-to{
+    opacity: 0;
+  }
+  .msg-enter-to,
+  .msg-leave-from{
+    opacity: 1;
+  }
+  .msg-enter-active,
+  .msg-leave-active{
+    transition: opacity 2s ease;
+  }
+</style>
